aca517ec947ca7420a2befc114b03d41b9d4c885
abhishekkumar3333 <abhishekatter3@gmail.com>
2026-04-15 16:28:50 +0530

    feat: initialize server entry point with database connection and environment configuration

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 import "dotenv/config";
 import app from "./app.js";
 import connectDb from "./config/db.js";
+
 const startserver = async () => {
     try {
         await connectDb();
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
+
+        const PORT = process.env.PORT || 5000;
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
     } catch (error) {
         console.log(error);
         process.exit(1);
     }
-}
-startserver();
+};
+
+startserver();  
