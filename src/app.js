4248c3d0cddd5f356cb5570ae9d7d7e9e04809a0
abhishekkumar444 <abhishekatter3@gmail.com>
2026-05-08 10:30:19 +0530

    modified

--- src/app.js
+++ src/app.js
@@ -13,3 +13,9 @@ app.use("/api/v1", router);
 export default app;
 
 
+
+// PORT=3000
+// DB_URL=mongodb://abhishekkumar:abhi12345@ac-v8q8trd-shard-00-00.jzv9xqa.mongodb.net:27017,ac-v8q8trd-shard-00-01.jzv9xqa.mongodb.net:27017,ac-v8q8trd-shard-00-02.jzv9xqa.mongodb.net:27017/?ssl=true&replicaSet=atlas-5xaei8-shard-0&authSource=admin&appName=Cluster9
+// JWT_SECRET=Kj7xP9@Lm2$qW8!nZ5^rT1&vB4*Hs6
+// EMAIL_USER=abhishekatter3@gmail.com
+// PASS_USER=mruazyhicfvsuuyf
